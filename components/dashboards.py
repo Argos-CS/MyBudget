02fcadf2fad059172f688c4e4fb9686cdde444f7
Ice Man <140863320+Argos-CS@users.noreply.github.com>
2024-05-07 18:53:07 -0300

    Update dashboards.py

--- components/dashboards.py
+++ components/dashboards.py
@@ -10,11 +10,63 @@ import calendar
 from globals import *
 from app import app
 
+card_icon = {
+    "color": "white",
+    "textAlign": "center",
+    "fontSize": 30,
+    "margin": "auto",
+}
 
 # =========  Layout  =========== #
 layout = dbc.Col([
-        html.H5('Dashboards')    
-    ])
+    dbc.Row([
+
+        # Saldo Total    
+        dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Saldo'),
+                    html.H5('R$ 5400', id='p-saldo-dashboards', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+                dbc.Card([
+                    html.Div(className='fa fa-university', style=card_icon),
+                    color='warning',
+                    style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}    
+                )
+            ])
+        ], width=4),
+
+        # Receita
+         dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Receita'),
+                    html.H5('R$ 10000', id='p-receita-dashboards', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+                dbc.Card([
+                    html.Div(className='fa fa-smile-o', style=card_icon),
+                    color='sucess',
+                    style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}    
+                )
+            ])
+        ], width=4),
+
+        # Despesa
+         dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Depesa'),
+                    html.H5('R$ 4600', id='p-depesa-dashboards', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+                dbc.Card([
+                    html.Div(className='fa fa-meh-o', style=card_icon),
+                    color='danger',
+                    style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}    
+                )
+            ])
+        ], width=4)
+    ], style={'margin': '10px'})
+])
 
 
 
